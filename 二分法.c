c51aa331f37489cdac0be4f344130cc6268514eb
Cu1Tao <96221554+Cu1Tao@users.noreply.github.com>
2021-12-16 12:53:55 +0800

    Add files via upload

---
+++ 二分法.c
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int low=1,high=n;
+    int mid;
+    while(low<high)
+    {
+        mid=low+(high-low)/2;
+        if(isBadVersion(mid))
+        {
+            high = mid;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+    return high;
+}
